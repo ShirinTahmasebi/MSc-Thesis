f416fe5c90698bc48babeff44cd40691fc27bcb5
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2019-12-23 10:22:49 +0330

    #Fog Create logs for add attributes

--- Implementation/thesis-fog/test/createLogs.js
+++ Implementation/thesis-fog/test/createLogs.js
@@ -3,10 +3,13 @@ const getDeviceProfileContract = require("../utils/getContracts").getDeviceProfi
 const generateUUID = require("./utils").generateUUID;
 const getAddDeviceLogFilePath = require("./utils").getAddDeviceLogFilePath;
 const getAddDeviceLogRowTemplate = require("./utils").getAddDeviceLogRowTemplate;
+const getAddAttributeLogFilePath = require("./utils").getAddAttributeLogFilePath;
+const getAddAttributeLogRowTemplate = require("./utils").getAddAttributeLogRowTemplate;
 const web3 = getWeb3();
 const fs = require('fs');
 
 const baseDeviceModel = 10000;
+const accountAddress = "0xd22f9E46718d7f3A37698490E2373BE250e4150D";
 const txStageEnum = {
   TX_SUBMITTED: 'TX_SUBMITTED',
   TX_HASH_RECEIVED: 'TX_HASH_RECEIVED',
@@ -36,11 +39,10 @@ const createUsers = async () => {
 };
 
 const createAndLogAddDeviceMockTx = async (counter) => {
-  // Create/Reset log file
-  fs.writeFileSync(getAddDeviceLogFilePath(counter), "");
-
   const logFilePath = getAddDeviceLogFilePath(counter);
-  const accountAddress = "0xd22f9E46718d7f3A37698490E2373BE250e4150D";
+
+  // Create/Reset log file
+  fs.writeFileSync(logFilePath, "");
 
   for (let i = 0; i < counter; i++) {
     const txUUID = generateUUID();
@@ -101,6 +103,81 @@ const createAndLogAddDeviceMockTx = async (counter) => {
   }
 };
 
+const createAndLogAddAttributeMockTx = async (counter) => {
+  const logFilePath = getAddAttributeLogFilePath(counter);
+
+  // Create/Reset log file
+  fs.writeFileSync(logFilePath, "");
+
+  for (let i = 0; i < counter; i++) {
+    const txUUID = generateUUID();
+    const deviceId = baseDeviceModel + i + 1;
+    const now = new Date();
+    const blockNumber = await web3.eth.getBlockNumber();
+
+    fs.appendFile(
+      logFilePath,
+      getAddAttributeLogRowTemplate(now, now.getTime(), blockNumber, txUUID, deviceId, accountAddress, "-", "-", txStageEnum.TX_SUBMITTED),
+      (err) => {
+        if (err) throw err;
+      },
+    );
+
+    const minThreshold = 10;
+    const maxThreshold = 100;
+    const minViolationCount = 5;
+    const maxViolationCount = 10;
+    const minCritically = 1;
+    const maxCritically = 2;
+
+    deviceProfileInstance
+      .methods
+      .addAttributeToDeviceById(
+        deviceId, `Attribute Name ${deviceId}`,
+        minThreshold, minViolationCount, minCritically,
+        maxThreshold, maxViolationCount, maxCritically,
+      ).send({from: accountAddress})
+      .on('transactionHash', (hash) => {
+        const now = new Date();
+        fs.appendFile(
+          logFilePath,
+          getAddAttributeLogRowTemplate(now, now.getTime(), "-", txUUID, deviceId, accountAddress, hash, "-", txStageEnum.TX_HASH_RECEIVED),
+          (err) => {
+            if (err) throw err;
+          },);
+      })
+      .on('receipt', (receipt) => {
+        fs.appendFile(
+          logFilePath,
+          getAddAttributeLogRowTemplate(now, now.getTime(), receipt.blockNumber, txUUID, deviceId, accountAddress, receipt.transactionHash, receipt.gasUsed, txStageEnum.TX_RECEIPT_RECEIVED),
+          (err) => {
+            if (err) throw err;
+          },
+        );
+      })
+      .on('confirmation', (confirmationNumber, receipt) => {
+        if (confirmationNumber === 1) {
+          fs.appendFile(
+            logFilePath,
+            getAddAttributeLogRowTemplate(now, now.getTime(), receipt.blockNumber, txUUID, deviceId, accountAddress, receipt.transactionHash, receipt.gasUsed, txStageEnum.TX_RECEIPT_CONFIRMED_1),
+            (err) => {
+              if (err) throw err;
+            },
+          );
+        }
+      }).on('error', (receipt) => {
+        fs.appendFile(
+          logFilePath,
+          getAddAttributeLogRowTemplate(now, now.getTime(), receipt.blockNumber, txUUID, deviceId, accountAddress, receipt.transactionHash, receipt.gasUsed, txStageEnum.TX_ERROR),
+          (err) => {
+            if (err) throw err;
+          },);
+      },
+    );
+
+  }
+};
+
 const init = async () => {
   const shouldCreateUsers = false;
   shouldCreateUsers && createUsers();
@@ -117,7 +194,12 @@ const main = async () => {
   // await createAndLogAddDeviceMockTx(50);
   // await createAndLogAddDeviceMockTx(100);
   // await createAndLogAddDeviceMockTx(150);
-  await createAndLogAddDeviceMockTx(200);
+  // await createAndLogAddDeviceMockTx(200);
+
+  // await createAndLogAddAttributeMockTx(50);
+  // await createAndLogAddAttributeMockTx(100);
+  // await createAndLogAddAttributeMockTx(150);
+  await createAndLogAddAttributeMockTx(200);
 };
 
 
--- Implementation/thesis-fog/test/utils.js
+++ Implementation/thesis-fog/test/utils.js
@@ -11,6 +11,12 @@ const getAddDeviceLogRowTemplate = (date, timestamp, blockNumber, txUUID, device
   return `Date=${date}  timestamp=${timestamp}  blockNumber=${blockNumber}  txUUID=${txUUID}  deviceId=${deviceId}  accountIndex=${accountAddress}  txHash=${txHash}  txStage=${txStage}  gasUsed=${gasUsed}\n`;
 };
 
+const getAddAttributeLogFilePath = (usersCount) => `${__dirname}/logs/add_attribute/log_addAttribute_${usersCount}.txt`;
+
+const getAddAttributeLogRowTemplate = (date, timestamp, blockNumber, txUUID, deviceId, accountAddress, txHash, gasUsed, txStage) => {
+  return `Date=${date}  timestamp=${timestamp}  blockNumber=${blockNumber}  txUUID=${txUUID}  deviceId=${deviceId}  accountIndex=${accountAddress}  txHash=${txHash}  txStage=${txStage}  gasUsed=${gasUsed}\n`;
+};
+
 const getFetchDevicesLogFilePath = (usersCount) => `${__dirname}/logs/fetch_devices/log_fetchDevices_${usersCount}.txt`;
 
 const getFetchDevicesLogRowTemplate = (date, timestamp, blockNumber, txUUID, deviceId, accountAddress, txStage) => {
@@ -21,6 +27,8 @@ module.exports = {
   generateUUID,
   getAddDeviceLogFilePath,
   getAddDeviceLogRowTemplate,
+  getAddAttributeLogFilePath,
+  getAddAttributeLogRowTemplate,
   getFetchDevicesLogFilePath,
   getFetchDevicesLogRowTemplate,
 };
