159380ca35b35a5ea2dc0ae38dd5b9f056165999
ShirinTahmasebi <shirin_tahmasebi94@yahoo.com>
2019-12-25 20:16:22 +0330

    #SmartContract Deploy to Ropsten

--- Implementation/thesis-smartContract/truffle-config.js
+++ Implementation/thesis-smartContract/truffle-config.js
@@ -1,24 +1,42 @@
-const path = require("path");
+// const path = require("path");
+//
+// module.exports = {
+//   // See <http://truffleframework.com/docs/advanced/configuration>
+//   // to customize your Truffle configuration!
+//   // networks: {
+//   //   develop: {
+//   //     port: 8545
+//   //   }
+//   // }
+//   // contracts_build_directory: path.join(__dirname, "../thesis-front/src/contracts"),
+//   compilers: {
+//     solc: {
+//       version: "0.5.12",
+//     },
+//   },
+//   networks: {
+//     development: {
+//       host: "127.0.0.1",
+//       port: 7545,
+//       network_id: "*",
+//     },
+//   },
+// };
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  // networks: {
-  //   develop: {
-  //     port: 8545
-  //   }
-  // }
-  // contracts_build_directory: path.join(__dirname, "../thesis-front/src/contracts"),
   compilers: {
     solc: {
       version: "0.5.12",
     },
   },
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*",
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider("banner push rocket mutual age agree thrive private goat video anchor mercy", "https://ropsten.infura.io/v3/044d4f53bdb7433f8e0442085ef38ea6");
+      },
+      network_id: 3,
     },
   },
 };
